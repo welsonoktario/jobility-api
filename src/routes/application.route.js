8341d675e5904d8f2c6e5ac912caa0ae33448af3
yovindoardana <yovindoardanaa@gmail.com>
2023-11-01 22:13:18 +0700

    implement verify token in application post and put

--- src/routes/application.route.js
+++ src/routes/application.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { applicationController } = require('../controllers');
+const verifyToken = require('../utils/verifyJwt');
 
 const router = express.Router();
 
 router.get('/', applicationController.findAll);
 router.get('/:id', applicationController.find);
-router.post('/', applicationController.create);
-router.put('/:id', applicationController.update);
+router.post('/', verifyToken, applicationController.create);
+router.put('/:id', verifyToken, applicationController.update);
 
 module.exports = router;
