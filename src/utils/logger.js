6a0cc2affec497e01b1f81f548deeb267557a8b1
Welson Oktario <welsonoktario@yahoo.com>
2023-11-09 02:03:31 +0700

    Fix log 3

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,15 +1,33 @@
 const log4js = require('log4js');
 
 log4js.configure({
+  disableClustering: true,
   appenders: {
     access: {
       type: 'dateFile',
-      filename: 'logs/access.log',
+      base: 'logs/access.log',
       pattern: '-yyyy-MM-dd',
+      category: 'http',
+    },
+    app: {
+      type: 'dateFile',
+      filename: 'logs/app.log',
+      maxLogSize: 10485760,
+      numBackups: 3,
+    },
+    errorFile: {
+      type: 'dateFile',
+      filename: 'logs/errors.log',
+    },
+    errors: {
+      type: 'logLevelFilter',
+      level: 'ERROR',
+      appender: 'errorFile',
     },
   },
   categories: {
-    default: { appenders: ['access'], level: 'DEBUG' },
+    default: { appenders: ['app', 'errors'], level: 'DEBUG' },
+    http: { appenders: ['access'], level: 'DEBUG' },
   },
 });
 
